fda546042fe25fd411919f90fe73def86334980e
Aamir Shafi <ashafi@nvidia.com>
2024-12-13 02:53:34 +0200

    added impl for ucc_tl_ucp_alltoall_pairwise_ca_init

--- src/components/tl/ucp/alltoall/alltoall.c
+++ src/components/tl/ucp/alltoall/alltoall.c
@@ -75,6 +75,22 @@ out:
     return status;
 }
 
+ucc_status_t ucc_tl_ucp_alltoall_pairwise_ca_init(ucc_base_coll_args_t *coll_args,
+                                                  ucc_base_team_t *team,
+                                                  ucc_coll_task_t **task_h)
+{
+    ucc_tl_ucp_team_t *tl_team = ucc_derived_of(team, ucc_tl_ucp_team_t);
+    ucc_tl_ucp_task_t *task;
+    ucc_status_t       status;
+
+    ALLTOALL_TASK_CHECK(coll_args->args, tl_team);
+    task                 = ucc_tl_ucp_init_task(coll_args, team);
+    *task_h              = &task->super;
+    status = ucc_tl_ucp_alltoall_pairwise_init_common(task);
+out:
+    return status;
+}
+
 ucc_status_t ucc_tl_ucp_alltoall_onesided_init(ucc_base_coll_args_t *coll_args,
                                                ucc_base_team_t *team,
                                                ucc_coll_task_t **task_h)
